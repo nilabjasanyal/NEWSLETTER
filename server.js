14f3d4d6bfbfe4b6a4879d8f73021145f3201741
nilabjasanyal <nilabja.sanyal@gmail.com>
2022-01-26 00:49:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,48 +22,73 @@ app.post("/", function(req, res){
     var lastName = req.body.lname;
     var email = req.body.email;
     
-    var data = {
-        members: [
-            {
-            email_address: email,
-            status: "subscribed",
-            merge_fields: {
-                FNAME: firstName,
-                LNAME: lastName
-                }
-            }
-        ]
-    }
+    const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
 
     var jsonData = JSON.stringify(data);
+    const url="https://us20.api.mailchimp.com/3.0/lists/" + listID;
+    const options = {
+  method: "POST",
+  auth: "yungseo1:"+apiKey
+};
+//     var options = {
+//         url: "https://us20.api.mailchimp.com/3.0/lists/" + listID,
+//         method: "POST",
+//         headers: {
+//             "auth": "anagutjor:" + apiKey
+//         },
+//         body: jsonData
+//     }
 
-    var options = {
-        url: "https://us20.api.mailchimp.com/3.0/lists/" + listID,
-        method: "POST",
-        headers: {
-            "auth": "anagutjor:" + apiKey
-        },
-        body: jsonData
-    }
+//     request(options, function(error, response, body){
+//         if(error){
+//             res.sendFile(__dirname + "/failure.html");
+//         } else {
+//             console.log(response.statusCode);
+//                 if(response.statusCode === 200){
+//                     res.sendFile(__dirname + "/success.html");
+//                 } else {
+//                     res.sendFile(__dirname + "/failure.html");
+//                 }
+//         }
+//     })
+// })
 
-    request(options, function(error, response, body){
-        if(error){
-            res.sendFile(__dirname + "/failure.html");
-        } else {
-            console.log(response.statusCode);
-                if(response.statusCode === 200){
-                    res.sendFile(__dirname + "/success.html");
-                } else {
-                    res.sendFile(__dirname + "/failure.html");
-                }
-        }
-    })
-})
+// app.post("/failure", function(req, res){
+//     res.redirect("/");
+// })
 
-app.post("/failure", function(req, res){
-    res.redirect("/");
-})
-
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Server is listening on port 3000");
-})
+// app.listen(process.env.PORT || 3000, function(){
+//     console.log("Server is listening on port 3000");
+// })
+    const request = https.request(url, options, function(response) {
+  if (response.statusCode === 200) {
+    res.sendFile(__dirname + "/success.html");
+  } else {
+    res.sendFile(__dirname + "/failure.html");
+  }
+  response.on("data", function(data) {
+    console.log(JSON.parse(data));
+  })
+});
+request.write(jsonData);
+request.end();
+});
+ 
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+ 
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running on port 3000.");
+}); 
